b9863f3c7686ce9d0be2b9bd1f110e3b998e1775
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-11-04 15:54:31 -0500

    Almost forgot to create a repository and commit

---
+++ 316DOMLAB.js
@@ -0,0 +1,29 @@
+//Putting this here for good practice until I am fully familar and confident
+console.log('Checking My Console for Errors')
+
+//Select and chache'ing (spell checking 🤦🏾‍♀️) the <main>Element</main>
+const mainEl = document.querySelector('main');
+
+//Adding background color (Use Var to access directly)
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+//Changing the content of mainEl to display <h1>DOM Manipulation</h1>
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+//Adding the flex-ctr class to mainEl
+mainEl.classList.add('flex-ctr');
+
+//Select and caching(spell checked... there's no "e" in chaching.. Who KNEW?🙃) the <nav id="top-menu">
+const menuEl = document.querySelector('menu');
+
+//Chaning height of top menu to be 100%
+topMenuEl.style.height = '100%';
+
+
+
+
+
+
+
+
+
